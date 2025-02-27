33e4706489557633597a2002a10b13186dd471ce
Shivank Singh <shivankrathore.31@gmail.com>
2025-02-27 14:43:43 +0530

    add register controller

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -18,6 +18,7 @@ const uploadonClodinary = async (localFilePath) => {
         // file has been uploaded successfully
         console.log("file is uploaded on cloudinary ",response.url);
         return response
+        
     } catch (error) {
         fs.unlinkSync(localFilePath) // remove the locally saved temporary file as operation got failed
         return null;
